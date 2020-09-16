e50705956fbfcf605f65878d4dc6f9ffbc54abce
Gabriel Henrique de Araujo Meira <54187292+ghmeira@users.noreply.github.com>
2020-09-16 19:39:33 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,26 @@
+let buttonCalcular = document.querySelector("#btnC") // botão "Calcular" como variavel
+
+
+buttonCalcular.onclick = function principal(){
+    let nomeVariavel = document.querySelector("#nomeVar").value
+    let listaDados = document.querySelector("#textoDados").value.split(" ") // valores das variaveis tranformados em lista
+    let verifica_1 // recebe o valor dos radios 1
+    let verifica_2 // recebe o valor dos radios 2
+
+    // verifica qual radio da variavel 1 foi selecionado
+    var radios_1 = document.querySelector("#tipoVariavel");
+    for (var i = 0; i < radios_1.length; i++) {
+        if (radios_1[i].checked) {
+            verifica_1 = radios_1[i].value
+        }
+    }
+
+    // verifica qual radio da variavel 2 foi selecionado
+    var radios_2 = document.querySelector("#tipoVariavel2");
+    for (var i = 0; i < radios_2.length; i++) {
+        if (radios_2[i].checked) {
+            verifica_2 = radios_2[i].value
+        }
+    }
+
+}
