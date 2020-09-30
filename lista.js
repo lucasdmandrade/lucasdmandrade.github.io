2b4612953329f628c6886aa9cd56701fb06ad3db
brufis <faleiros.b@gmail.com>
2020-09-30 02:09:24 +0000

    lista

---
+++ lista.js
@@ -0,0 +1,19 @@
+let linha = [
+    "Ensino Superior", 
+    "Ensino Médio", 
+    "Ensino Fundamental", 
+    "Ensino Fundamental", 
+    "Ensino Médio", "Ensino Superior", 
+    "Ensino Superior", 
+    "Ensino Superior", 
+    "Ensino Médio", 
+    "Ensino Médio", 
+    "Ensino Médio", 
+   "Ensino Fundamental",
+    "Ensino Médio", 
+    "Ensino Superior", 
+    "Ensino Fundamental",
+    "Ensino Médio", 
+    "Ensino Médio", 
+    "Ensino Médio"
+]
